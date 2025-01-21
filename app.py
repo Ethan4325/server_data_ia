038a63bbbe06165ae5b324800db254ff27bf60c8
Ethan <ethanethan23347@gmail.com>
2025-01-21 15:45:05 +0100

    New

--- app.py
+++ app.py
@@ -1,49 +1,57 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-# Base de données de livres
+
 books = [
-   {"id": 1, "title": "Harry Potter and the Sorcerer's Stone", "author": "J.K. Rowling", "year": 1997},
-   {"id": 2, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925},
-   {"id": 3, "title": "1984", "author": "George Orwell", "year": 1949},
-   {"id": 4, "title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960},
-   {"id": 5, "title": "A Promised Land", "author": "Barack Obama", "year": 2020}
+    {"id": 1, "title": "Harry Potter and the Sorcere's Stone","author":"J.K.Rowling","year":1997},
+    {"id": 2, "title": "The Great Gatsby","author":"F.Scott Fitzgerald","year":1925},
+    {"id": 3, "title": "1984","author":"George Orwell","year":1949},
+    {"id": 4, "title": "To Kill a Mockingbird","author":"Harper Lee","year":1960},
+    {"id": 5, "title": "A Promised Land","author":"Barrack Obama","year":2020}
 ]
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=("GET","POST"))
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
-@app.route('/search', methods=['GET'])
-def search():
+# @app.route('/search', methods=["GET"])
+# def search():
+#     query = request.args.get('query')
+#     date = request.args.get('date')
+#     return f'Recherche : {query}'
 
-   autorFromSearch = request.args.get('author')
-   exist = False
-   # Recherche d'un livre via le nom de l'auteur
-   for livre in books:
-      # verification du nom de l'auteur
-      if livre['author'] == autorFromSearch:
-         exist = True
-         return f'Livre : {livre['title']} ( {livre['year']})'
+@app.route('/books', methods=["GET"])
+def book():
 
-   if exist == False:
-       return "Aucun livre trouvé!"
+    author = request.args.get('author')
+    exist = False
+
+    for livre in books:
+        if livre['author'] == author:
+            exist = True
+            return f"livre : {livre['title']} ( {livre['year']})"
+
+    if exist == False:
+        return "cherche mieux"
+    
+@app.route('/add_book', methods=["POST"])
+def add_book():
+    titre = request.form.get('title')
+    auteur = request.form.get('author')
+    annee = request.form.get('year')
+
+    books.append(
+        {"id": len (books) + 1, "title": titre,"author":auteur,"year":annee}
+    )
+    return f'titre: {titre} | auteur : {auteur} | annee : {annee}'
 
 
-@app.route('/add_book', methods=['POST'])
-def addBook():
-     
-   # Recuperation des paramètres
-   titre = request.form.get('title')
-   auteur = request.form.get('author')
-   annee = request.form.get('year')
 
-   books.append(
-       {"id": len(books)+1, "title": titre, "author": auteur, "year": annee}
-   )
-     
-   return f'titre: {titre} | auteur : {auteur} | annee : {annee}'
+# @app.route('/login', methods=["POST"])
+# def login():
+#     nom = request.form.get('username')
+#     motdepasse = request.form.get('motdepasse')
+#     return f'Nom : {nom} / Mot de passe = {motdepasse}'
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     app.run(host='0.0.0.0', port=80)
